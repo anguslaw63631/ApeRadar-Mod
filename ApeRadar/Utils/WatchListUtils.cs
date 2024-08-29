32e2f4e32248cd0c5d309029b09986dc77acdf97
anguslaw63631 <anguslaw63631@gmail.com>
2024-08-29 10:04:12 +0800

    Clone from source(V2.1.0) Build 20230624

---
+++ ApeRadar/Utils/WatchListUtils.cs
@@ -0,0 +1,57 @@
+﻿using ApeRadar.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace ApeRadar.Utils
+{
+    static internal class WatchListUtils
+    {
+        public static void CreateNewWatchList(string filename)
+        {
+            using FileStream fs = new(filename, FileMode.Create, FileAccess.Write, FileShare.ReadWrite | FileShare.Delete);
+            using StreamWriter sw = new(fs);
+            JObject JObjectWatchList = JsonUtils.Parse("{\"RU\":{},\"EU\":{},\"NA\":{},\"ASIA\":{},\"CN\":{}}");
+            sw.WriteLine(JsonConvert.SerializeObject(JObjectWatchList, Formatting.Indented));
+        }
+
+        public static JObject ReadWatchList(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                CreateNewWatchList(filename);
+            }
+            using FileStream fs = new(filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete);
+            using StreamReader sr = new(fs);
+            string strWatchList = sr.ReadToEnd();
+            return JsonUtils.Parse(strWatchList);
+        }
+
+        public static void SaveWatchList(Player p, string filename)
+        {
+            JObject JObjectWatchList = ReadWatchList(filename);
+
+            if (JObjectWatchList[ServerExt.GetNameByServer(p.Server)]!.SelectToken(p.ID) != null)
+            {
+                if (p.WatchStatus == WatchStatus.NONE)
+                {
+                    JObject? JObjectToUpdate = JObjectWatchList[ServerExt.GetNameByServer(p.Server)] as JObject;
+                    JObjectToUpdate!.Remove(p.ID);
+                }
+                else
+                {
+                    JObjectWatchList[ServerExt.GetNameByServer(p.Server)]![p.ID]!["status"] = WatchStatusExt.GetNameByStatus(p.WatchStatus);
+                }
+            }
+            else
+            {
+                JObject JObjectPlayer = JsonUtils.Parse($"{{\"name\": \"{p.Name}\",\"status\": \"{WatchStatusExt.GetNameByStatus(p.WatchStatus)}\"}}");
+                JObject? JObjectToUpdate = JObjectWatchList[ServerExt.GetNameByServer(p.Server)] as JObject;
+                JObjectToUpdate!.Add(p.ID, JObjectPlayer);
+            }
+            using FileStream fs = new(filename, FileMode.Truncate, FileAccess.Write, FileShare.ReadWrite | FileShare.Delete);
+            using StreamWriter sw = new(fs);
+            sw.WriteLine(JsonConvert.SerializeObject(JObjectWatchList, Formatting.Indented));
+        }
+    }
+}
